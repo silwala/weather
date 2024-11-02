4434afc39aee846d770383a82ee760914ca116de
Aditya Silwal <silwala@sheridancollege.ca>
2024-11-01 22:32:13 -0400

    add functionality to search button

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,17 @@
 const celsius = document.querySelector(".celsius");
 const farhenheit = document.querySelector(".farhenheit");
+const search = document.querySelector(".search");
+const searchQuery = document.querySelector(".search-query");
+const APIKey = "9ZX4M2DDTAWVY76JZT5WKWX7E";
 
 celsius.addEventListener("click", () => changeUnit('c'))
 farhenheit.addEventListener("click", () => changeUnit('f'))
+search.addEventListener("click", () => searchClick())
+searchQuery.addEventListener("keydown", (e) => {
+    if (e.keyCode === 13){
+        search.click();
+    }
+})
 
 function changeUnit(unit){
     if(unit === 'f'){
@@ -14,3 +23,18 @@ function changeUnit(unit){
         farhenheit.classList.remove("selected");
     }
 }
+
+function searchClick(){
+    if(searchQuery.value.trim() === ""){
+        searchQuery.focus();
+    }
+    else{
+        fetchWeather(searchQuery.value);
+    }
+
+}
+
+function fetchWeather(location){
+    alert(location);
+}
+
