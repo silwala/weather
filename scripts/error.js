141d375645b21586a8558982017ba6ce85027c7b
Aditya Silwal <silwala@sheridancollege.ca>
2024-11-07 02:24:15 -0500

    wrap error js operations in a function

--- scripts/error.js
+++ scripts/error.js
@@ -1,20 +1,24 @@
-const code = document.querySelector(".error-code");
-const description = document.querySelector(".error-description");
-const params = new URLSearchParams(window.location.search);
-const errorMessage = params.get("message");
-code.textContent = isCodeNumber(errorMessage) ? errorMessage : "Oops!";
-
-if(errorMessage === "400"){
-    description.textContent = "Couldn't find a result for your search"
-}
-else if(errorMessage === "404"){
-    description.textContent = "The page doesn't exist. Please Come back later."
-}
-else{
-    description.textContent = "Something went wrong. Please Come back later."
-
+function updateDisplay(){
+    const code = document.querySelector(".error-code");
+    const description = document.querySelector(".error-description");
+    const params = new URLSearchParams(window.location.search);
+    const errorMessage = params.get("message");
+    code.textContent = isCodeNumber(errorMessage) ? errorMessage : "Oops!";
+    
+    if(errorMessage === "400"){
+        description.textContent = "Couldn't find a result for your search"
+    }
+    else if(errorMessage === "404"){
+        description.textContent = "The page doesn't exist. Please Come back later."
+    }
+    else{
+        description.textContent = "Something went wrong. Please Come back later."
+    
+    }
 }
 
 function isCodeNumber(code) {
     return !isNaN(code) && code.trim() !== "";
 }
+
+updateDisplay();
