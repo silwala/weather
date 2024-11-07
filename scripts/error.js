1efc6f7ef305cbfc7fdfc95963d33c03ac689335
Aditya Silwal <silwala@sheridancollege.ca>
2024-11-07 02:21:52 -0500

    display search bar under app name and switch button so it is more responsive and works for phones

--- scripts/error.js
+++ scripts/error.js
@@ -2,8 +2,8 @@ const code = document.querySelector(".error-code");
 const description = document.querySelector(".error-description");
 const params = new URLSearchParams(window.location.search);
 const errorMessage = params.get("message");
-code.textContent = errorMessage || "An unexpected error occurred.";
+code.textContent = isCodeNumber(errorMessage) ? errorMessage : "Oops!";
 
 if(errorMessage === "400"){
     description.textContent = "Couldn't find a result for your search"
-}
+  }
