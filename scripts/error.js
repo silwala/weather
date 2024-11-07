e9a1e3cbd18f3103c448d72ce34397b98d4d548b
Aditya Silwal <silwala@sheridancollege.ca>
2024-11-07 02:22:30 -0500

    add more checks in error page

--- scripts/error.js
+++ scripts/error.js
@@ -6,4 +6,15 @@ code.textContent = isCodeNumber(errorMessage) ? errorMessage : "Oops!";
 
 if(errorMessage === "400"){
     description.textContent = "Couldn't find a result for your search"
-  }
+}
+else if(errorMessage === "404"){
+    description.textContent = "The page doesn't exist. Please Come back later."
+}
+else{
+    description.textContent = "Something went wrong. Please Come back later."
+
+}
+
+function isCodeNumber(code) {
+    return !isNaN(code) && code.trim() !== "";
+}
